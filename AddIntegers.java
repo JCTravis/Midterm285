e33479b9d1cf3cf491c4994b93858ffb2f6f5c5b
Jacob <jctravis@umich.edu>
2022-03-10 15:14:39 -0500

    added body function

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,5 +2,17 @@ public class AddInteger
 {
 	public Integer(){
 		system.out.println("Add Integer");
+		system.out.println()
+	}
+
+	public static void main(String[] args) {
+
+			int first = 15;
+			int second = 25;
+
+			System.out.println("Enter two numbers: " + first + " " + second);
+			int sum = first + second;
+
+			System.out.println("The sum is: " + sum);
 	}
 }
